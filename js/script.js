61b073ec499352e773340be5fa54cc3e46c59777
CJAX.Dev <jackson102890@gmail.com>
2024-09-24 13:18:51 -0600

    finish slider javascript and css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 // Variables
-
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
 // Functions
-
+function startSlideshow() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add("show-slide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+function showSlide(index) {
+    if(index >= slides.length) {
+        slideIndex = 0;
+    }
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach(slide => {
+        slide.classList.remove("show-slide");
+    });
+    slides[slideIndex].classList.add("show-slide");
+}
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+function prevSlide() {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
 // Events
+document.addEventListener("DOMContentLoaded", startSlideshow);
